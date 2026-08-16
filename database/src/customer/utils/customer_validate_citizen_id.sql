caa9bafe1eae202a1f06111f761b7dccd43ee43e
KhangHuynh <khanghb.ti.2124@gmail.com>
2026-08-16 22:49:02 +0700

    add customer helper functions

---
+++ database/src/customer/utils/customer_validate_citizen_id.sql
@@ -0,0 +1,23 @@
+USE BankingSystem
+GO
+
+/*
+    Function : fn_customer_validate_citizen_id
+    Description : Check if the citizen ID is valid.
+*/
+CREATE OR ALTER FUNCTION fn_customer_validate_citizen_id
+    (@citizen_id VARCHAR(20))
+RETURNS BIT
+AS
+BEGIN
+    DECLARE @is_valid BIT = 0;
+
+    IF EXISTS (
+        SELECT 1
+        FROM Customer
+        WHERE citizen_id = @citizen_id
+    )
+        SET @is_valid = 1;
+
+    RETURN @is_valid;
+END
