97040ead7456a84d6669727ad9b97fd3a902475e
KhangHuynh <khanghb.ti.2124@gmail.com>
2026-08-18 19:40:00 +0700

    Update file mask_phone_number

--- database/src/common/mask_phone_number.sql
+++ database/src/common/mask_phone_number.sql
@@ -1,1 +1,25 @@
-martcat: swift brown fox jumps
+USE BankingSystem;
+GO
+
+/*
+    Function: fn_mask_phone_number
+    Description: Masks a phone number by replacing the first 7 digits with asterisks (*) and preserving the last 3 digits.
+
+    Example: 0901234567 -> *******567
+
+    Input: VARCHAR(20) phone number
+    Output: VARCHAR(20) masked number
+*/
+CREATE OR ALTER FUNCTION dbo.fn_mask_phone_number
+    (@phone_number VARCHAR(20))
+RETURNS VARCHAR(20)
+AS
+BEGIN
+    IF @phone_number IS NULL
+        RETURN NULL;
+    DECLARE @phone_length INT = LEN(@phone_number);
+    IF @phone_length <= 7
+        RETURN REPLICATE('*', @phone_length);
+    RETURN REPLICATE('*', 7) + RIGHT(@phone_number, 3);
+END;
+GO
