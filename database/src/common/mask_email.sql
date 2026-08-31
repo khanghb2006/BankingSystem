1c4b30080d18470d4bffd5bf3d8fc392b5631b77
KhangHuynh <khanghb.ti.2124@gmail.com>
2026-08-31 16:13:09 +0700

    Update common helpers function

--- database/src/common/mask_email.sql
+++ database/src/common/mask_email.sql
@@ -1,7 +1,7 @@
 USE BankingSystem;
 GO
 
-/*
+/**
     Function: fn_mask_email
     Description: Masks an email address by preserving exactly the first 4 characters and replacing the remaining characters with asterisks (*).
 
@@ -10,9 +10,9 @@ GO
     Input: VARCHAR(255) email address
     Output: VARCHAR(255) masked email address
 */
-CREATE OR ALTER FUNCTION dbo.fn_mask_email
-    (@email VARCHAR(255))
-RETURNS VARCHAR(255)
+CREATE OR ALTER FUNCTION fn_mask_email
+    (@email NVARCHAR(100))
+RETURNS NVARCHAR(100)
 AS
 BEGIN
     IF @email IS NULL
