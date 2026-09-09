e3c1e6bd24f877acdafc123fa8bec22e74ad0682
KhangHuynh <khanghb.ti.2124@gmail.com>
2026-09-09 16:00:47 +0700

    Update repo

---
+++ backend/test.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.awt.*;
+import java.text.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
